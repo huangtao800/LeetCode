c2f46b37efb1245ec1508451fafbee5246857911
huangtao800 <huangtao800@gmail.com>
2015-07-23 20:51:36 +0800

    Reconstruct 76-2

--- 76-2.cpp
+++ 76-2.cpp
@@ -5,39 +5,46 @@
 #include <climits>
 
 using namespace std;
-
+/**
+* General idea: If there are still more characters not matched (required > 0), increment right. Otherwise, increment left.
+**/ 
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.size() == 0 || t.size() == 0) return "";
-        vector<int> remaining(128, 0);
+        if(s.size()==0 || t.size()==0) return "";
         int required = t.size();
-        for (int i = 0; i < required; i++) remaining[t[i]]++;
-        // left is the start index of the min-length substring ever found
-        int min = INT_MAX, start = 0, left = 0, i = 0;
-        while(i <= s.size() && start < s.size()) {
-            if(required) {
-                if (i == s.size()) break;
-                remaining[s[i]]--;
-                if (remaining[s[i]] >= 0) required--;
-                i++;
-            } else {
-                if (i - start < min) {
-                    min = i -start;
-                    left = start;
+        vector<int> remain(128,0);
+        int minWindowLength = INT_MAX;
+        for(int i=0;i<t.size();i++){
+            remain[t[i]]++;
+        }
+        
+        int left = 0, right = 0;
+        int start = 0;
+        while(right <= s.size() && left < s.size()){
+            if(required){
+                if(right ==s.size())    break;
+                remain[s[right]]--;
+                if(remain[s[right]] >= 0)   required--;
+                right++;
+            }else{
+                if(right - left < minWindowLength){
+                    start = left;
+                    minWindowLength = right - left;
                 }
-                remaining[s[start]]++;
-                if (remaining[s[start]] > 0) required++;
-                start++;
+                remain[s[left]]++;
+                if(remain[s[left]] > 0)    required++;
+                left++;
             }
         }
-        return min == INT_MAX? "" : s.substr(left, min);
+        
+        return minWindowLength==INT_MAX?(""):s.substr(start, minWindowLength);
     }
 };
 
 int main(){
 	string s = "cbbbbbbba";
-	string t = "ab";
+	string t = "";
 	Solution solution;
 	string result = solution.minWindow(s, t);
 	cout << result << endl;
